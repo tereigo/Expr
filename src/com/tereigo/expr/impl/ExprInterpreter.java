994f8067ef0e9af3ed385da380d9850fdb7495d0
Igor Terentiev <iterentiev@gmail.com>
2024-09-13 21:48:19 +0100

    Code cleanup

--- src/com/tereigo/expr/impl/ExprInterpreter.java
+++ src/com/tereigo/expr/impl/ExprInterpreter.java
@@ -39,10 +39,8 @@ final class ExprInterpreter implements Expr.Visitor<Variant> {
 
     @Override
     public Variant visitBinaryExpr(final Expr.Binary expr) {
-        // TODO: why they are outside of try?
         final Variant left = evaluate(expr.left);
         final Variant right = evaluate(expr.right);
-
         try {
             switch (expr.operator.type) {
                 case EQUAL_EQUAL:
@@ -93,7 +91,6 @@ final class ExprInterpreter implements Expr.Visitor<Variant> {
 
     @Override
     public Variant visitInOperator(final Expr.InOperator expr) {
-        // TODO: why it is outside of try?
         final Variant operand = evaluate(expr.operand);
         try {
             for (int i = 0; i < expr.values.size(); i++) {
@@ -112,7 +109,6 @@ final class ExprInterpreter implements Expr.Visitor<Variant> {
 
     @Override
     public Variant visitWithinOperator(final Expr.WithinOperator expr) {
-        // TODO: why it is outside of try?
         final Variant operand = evaluate(expr.operand);
         try {
             final Variant minVal = evaluate(expr.min);
@@ -132,7 +128,6 @@ final class ExprInterpreter implements Expr.Visitor<Variant> {
 
     @Override
     public Variant visitBetweenOperator(final Expr.BetweenOperator expr) {
-        // TODO: why it is outside of try?
         final Variant operand = evaluate(expr.operand);
         try {
             final Variant minVal = evaluate(expr.min);
